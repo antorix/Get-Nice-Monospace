a099145f334eef1c2bda4f833a6df9fb76102471
Anton Chivchalov <antorix@gmail.com>
2017-09-28 08:24:49 +0300

    Add files via upload

---
+++ get nice monotype.py
@@ -0,0 +1,20 @@
+import os
+def setFont():
+    """ Obtain the nicest monotype font from the system and return it"""
+    fonts=[]
+    if os.name=="nt":
+        try:
+            osFonts=os.listdir("C:/WINDOWS/fonts")
+            if "LiberationMono-Regular.ttf" in osFonts: fonts.append("Liberation Mono")
+            if "DejaVuSansMono_0.ttf" in osFonts: fonts.append("DejaVu Sans Mono")
+            if "lucon.ttf" in osFonts: fonts.append("Lucida Console")
+            if "cousine-regular.ttf" in osFonts: fonts.append("Cousine")
+            if "firamono-regular.ttf" in osFonts: fonts.append("Fira Mono")
+            if "PTM55F.ttf" in osFonts: fonts.append("PT Mono")
+            if "ubuntumono-r.ttf" in osFonts: fonts.append("Ubuntu Mono")            
+        except: print("no good font found on Windows")
+    fonts.append("Courier New")
+    return fonts[0]
+
+if __name__=="__main__":
+    print(setFont())
